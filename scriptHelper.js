05cfad60322151c050e09423542048f145d58ef2
Lisa Ulricksen <LisaHome@lisas-imac.attlocal.net>
2022-02-14 11:40:30 -0600

    changed pilot copilot

--- scriptHelper.js
+++ scriptHelper.js
@@ -75,13 +75,15 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
   ) {
     alert('Enter a valid number');
   }
+  pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`;
+  copilotStatus.innerHTML = `Co-pilot ${copilot} is ready for launch`;
 
   if (Number(fuelLevel) < 10000) {
     list.style.visibility = 'visible';
     launchStatus.style.color = 'red';
     launchStatus.innerHTML = `Shuttle Not Ready for Launch`;
-    pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`;
-    copilotStatus.innerHTML = `Co-pilot ${copilot} is ready for launch`;
+    //pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`;
+    //copilotStatus.innerHTML = `Co-pilot ${copilot} is ready for launch`;
     fuelStatus.innerHTML = `Fuel level too low for launch`;
     cargoLevel.innerHTML = `Cargo mass too heavy for launch`;
   }
@@ -90,8 +92,7 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
     list.style.visibility = 'visible';
     launchStatus.style.color = 'red';
     launchStatus.innerHTML = `Shuttle Not Ready for Launch`;
-    pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`;
-    copilotStatus.innerHTML = `Co-pilot ${copilot} is ready for launch`;
+    
     fuelLevel.innerHTML = `Fuel level too low for launch`;
     cargoStatus.innerHTML = `Cargo mass too heavy for launch`;
   }
@@ -100,8 +101,8 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
     list.style.visibility = 'visible';
     launchStatus.style.color = 'green';
     launchStatus.innerHTML = `Shuttle is Ready for Launch`;
-    pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`;
-    copilotStatus.innerHTML = `Co-pilot ${copilot} is ready for launch`;
+    //pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`;
+    //copilotStatus.innerHTML = `Co-pilot ${copilot} is ready for launch`;
     fuelLevel.innerHTML = `Fuel level high enough for launch`;
     cargoLevel.innerHTML = `Cargo mass low enough for launch`;
   }
